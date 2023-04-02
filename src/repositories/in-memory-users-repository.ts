eac22539609b41101e2026d5141f0a5c2dc4fc56
Daniel Souza <danielsouza51764@gmail.com>
2023-04-02 14:17:45 -0300

    Caso de Uso e Design Patterns - Interface do repositório

--- src/repositories/in-memory-users-repository.ts
+++
@@ -1,9 +0,0 @@
-import { Prisma } from '@prisma/client'
-
-export class InMemoryUsersRepository {
-  public users: any = []
-
-  async crate(data: Prisma.UserCreateInput) {
-    this.users.push(data)
-  }
-}
