1df263ccbca87895787f816a2cc0f3b36aa86327
Daniel Souza <danielsouza51764@gmail.com>
2023-04-04 12:37:53 -0300

    Implementando casos de uso - TDD & Mocking

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -5,11 +5,23 @@ import { randomUUID } from 'crypto'
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = []
 
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const checkInOnSameDate = this.items.find(
+      (checkIn) => checkIn.user_id === userId,
+    )
+
+    if (!checkInOnSameDate) {
+      return null
+    }
+
+    return checkInOnSameDate
+  }
+
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
       id: randomUUID(),
-      userId: data.userId,
-      gymId: data.gymId,
+      user_id: data.user_id,
+      gym_id: data.gym_id,
       validate_at: data.validate_at ? new Date(data.validate_at) : null,
       created_at: new Date(),
     }
